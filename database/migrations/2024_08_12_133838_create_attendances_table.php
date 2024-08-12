aaa843959395f17b8adb8a3fcfa3d4e8e9ecaf12
raeed1993 <raeed1993@outlook.com>
2024-08-12 17:25:29 +0300

    - fix attendance 2

--- database/migrations/2024_08_12_133838_create_attendances_table.php
+++ database/migrations/2024_08_12_133838_create_attendances_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->foreignId('user_id');
             $table->foreignId('task_id')->nullable();
             $table->timestamp('start_time');
-            $table->timestamp('start_time');
+            $table->timestamp('end_time');
             $table->string('duration');
             $table->timestamps();
         });
