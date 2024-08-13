f716f19802ff98a6f06f176eacbdc2e5f87c26bf
raeed1993 <raeed1993@outlook.com>
2024-08-13 09:27:42 +0300

    - fix return company object in user model 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,7 +73,7 @@ class User extends Authenticatable
 
     public function getCompanyObjectAttribute()
     {
-        return $this->company->pluck('name', 'id');
+        return $this->company()->first()->pluck('name', 'id');
     }
 
 }
