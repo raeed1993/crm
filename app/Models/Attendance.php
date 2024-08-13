2c5064377867a07a7ad937866a5de92fc022e84e
raeed1993 <raeed1993@outlook.com>
2024-08-13 09:38:28 +0300

    - fix return company object in user model 5

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -18,4 +18,40 @@ class Attendance extends Model
         'start_time' => 'datetime',
         'end_time' => 'datetime',
     ];
+
+    public function task()
+    {
+        return $this->hasOne(Task::class, 'id', 'task_id');
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    public function getTaskObjectAttribute()
+    {
+        $task = $this->task;
+        if (!$task) {
+            return null;
+        }
+        return ['name' => $task->title, 'id' => $task->id];
+    }
+
+    public function getUserObjectAttribute()
+    {
+        $user = $this->user;
+        if (!$user) {
+            return null;
+        }
+        return ['name' => $user->name, 'id' => $user->id];
+    }
+
+    public function toArray()
+    {
+        $array = parent::toArray();
+        $array['user'] = $this->getUserObjectAttribute();
+        $array['task'] = $this->getTaskObjectAttribute();
+        return $array;
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -18,9 +18,31 @@ class Project extends Model
         'desc',
         'test_url',
         'real_url',
+        'company_id',
     ];
     protected $casts = [
         'start_date' => 'datetime',
         'end_date' => 'datetime'
     ];
+
+    public function company()
+    {
+        return $this->hasOne(Company::class, 'id', 'company_id');
+    }
+
+    public function getCompanyObjectAttribute()
+    {
+        $company = $this->company;
+        if (!$company) {
+            return null;
+        }
+        return ['name' => $company->name, 'id' => $company->id];
+    }
+
+    public function toArray()
+    {
+        $array = parent::toArray();
+        $array['company'] = $this->getCompanyObjectAttribute();
+        return $array;
+    }
 }
